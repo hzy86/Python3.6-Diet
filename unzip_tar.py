6a539071a82ab924992e46d3f7f16af8b0876e19
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 18:37:37 +0000

    Adding tarfile member sanitization to extractall()

--- unzip_tar.py
+++ unzip_tar.py
@@ -3,7 +3,48 @@ import tarfile
 filename = "faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz"
 if (filename.endswith("tar.gz")):
     with tarfile.open(filename, "r:gz") as tar:
-        tar.extractall()
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 elif (filename.endswith("tar")):
     with tarfile.open(filename, "r:") as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
